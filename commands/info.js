97f640004f0546fc35110aae735f6bddd0880c07
Killetx <47619686+Killetx@users.noreply.github.com>
2023-04-12 01:55:45 -0400

    v1.0 | lots and lots and lots of changes.
    I made these changes months ago, and am honestly unsure of what they're about. There are lots of new things.... It's using embeds now, it's crazy

--- commands/info.js
+++ commands/info.js
@@ -1,80 +1,136 @@
 
 const axios = require('axios');
-const fs = require('fs');
-const https = require('https');
 
 
-
-
-
-
-
-
-
-
-
-async function get_user(user_name) {//
-  axios.get('https://ch.tetr.io/api/users/' + user_name)//
-    .then(response => {//
-      var userIDFinal = "fuck thsi shit";//
-      //const awaitUserID = async() => {//
-        userIDFinal = response.data.data.user._id + ""; //
-        setTimeout(() => { console.log("end"); }, 5000);
-        console.log(userIDFinal);//returns the propper thing, but returns it too late, printing after "user result"
-        return userIDFinal; //this statement works but it runs too soon and sends "undefined" (i think it runs fine)
-      
-     // }//
-     // awaitUserID();//
-    })//
-  .catch(error => {//
-    console.log('Got an error', error);//
-  })//
-}//
-  
-  
-  //
-  // const usermanager = require('usermanager');
-  // usermanager.get_user('killet', function(user) {
-  //    message.channel.send("User ID: " + user.data.user._id);
-  // });
-
-
-
+function getAvatarURL(type, id, response) {
+  var revision;
+  switch (type) {
+    case 'avatar': revision = response.data.data.user.avatar_revision + "";    
+    case 'banner': revision = response.data.data.user.banner_revision + "";
+  }
+  if(!response.data.data.user.avatar_revision) return "https://branding.osk.sh/tetrio-anim.gif"; 
+  return "https://tetr.io/user-content/" + type + "s/" + id + ".jpg?rv={" + revision + "}";
+}
 
 module.exports = {
 
-    name: 'info',
+  name: 'info',
+  description: 'N/a',
 
-    
+  execute(message, args, embed, client) {
+    if(message.guild || message.author.id == "501885489683628036") {
+ 
+      if(args[1]) { 
+        axios.get('https://ch.tetr.io/api/users/' + args[1])
+ 
+        .then(response => {
+          
+          //declaring all things to be used fom the API call
+          //----------------------------------------------------
+          var userid = response.data.data.user._id;
+          var username = response.data.data.user.username;
+          var userType = response.data.data.user.role;
+          var gamesPlayed = response.data.data.user.gamesplayed;
+          var gamesWon = response.data.data.user.gameswon;
+          var gamesLost;
+          var WLRatio;
+          //----------------------------------------------------
 
-    description: 'N/a',
+          var rank = response.data.data.user.league.percentile_rank;
+          rank = rank.replace('+', 'p');
+          rank = rank.replace('-', 'm');
+          console.log(rank);
+          var fuckArc = rank;
 
-    execute(message, args) {
+          var rankEmoji = client.emojis.cache.find(emoji => emoji.name == rank + "_");
+          var rankString = rankEmoji.toString();
+          console.log(rankEmoji);
+          var WLPR = []; //WinLossPlayedRatio (win/loss is value 0, the other two are 1, and 2 respectively) | Declaring them as hidden initially, changes later:
+          WLPR[0] = '`hidden`';
+          WLPR[1] = '`hidden`';
+          WLPR[2] = '`hidden`';
+
+          if(gamesWon != -1) {
+            WLPR[0] = gamesWon + ' / `hidden`'
+            if(gamesPlayed == -1) return;
+            gamesLost = gamesPlayed - gamesWon;
+            WLPR[0] = gamesWon + ' / ' + gamesLost;
+            WLPR[1] = gamesPlayed;
+
+            WLRatio = gamesWon / gamesPlayed + '';
+      
+            if(!WLRatio[3]) WLRatio[3] = 0;       //checks if WLRATIO has a 2nd digit, and adds a 0 if not. Example: .4 instead of 40
+            WLPR[2] = WLRatio[2] + WLRatio[3] + "%";
+            if(WLRatio[2] == 0) WLPR[2] = WLRatio[3] + "%"; //removes the first 0 if present
+          }
+
+          
+          //Code to see if the country is hidden, can't "tolowercase" null, so it'll catch the error and set it to hidden, kinda stupid but it works.
+          var country;
+          try{
+            country = response.data.data.user.country.toLowerCase();
+          } catch (err) {
+            country = 'hidden'
+          }
+          
+
+          //Possible Hidden Values: Country, GamesPlayed, GamesWon, GameTime
+          
 
 
-        if(message.guild || message.author.id == "501885489683628036") {
 
-            if(args[1]) {
-                
+          //calls avatarURL
+          var avatarURL = getAvatarURL("avatar", userid, response);
+          //checks if the user has a bio, returns "" if not.
+          var userBio = "";
+          if(response.data.data.user.bio) var userBio = response.data.data.user.bio;
+          
+          var finalEmbed = embed
+          .spliceFields(0, 25)
+          .setDescription(userBio)
+          .setColor('#d726ff')
+          .setTitle(username + " :flag_" + country + ": " + rankString)
+          .setURL("https://ch.tetr.io/u/" + username)
+          .setThumbnail(avatarURL)
 
-                try {
-                  console.log('thru3');//
-                  //     const awaitUserIDfunk = async() => {//
-                  var userid = get_user(args[1]);
-                    
-                  console.log("user result:\n" + userid); //somehow prints before the function is entirely ran, and prints after "userid"
-                  //     }//
-                  //     awaitUserIDfunk();//
-                }  catch(error) {
+          //you could center this with a case: (gamesPlayed.length) which is autistic but possible
+          .addField("Games Won/Lost⠀⠀⠀", WLPR[0], true)
+          .addField("Games Played⠀⠀⠀", WLPR[1], true)
+          .addField("W/L Ratio", WLPR[2], true)
+          .addField("new", "line (2)", false)
+          .addField("line", "3", true)
 
-                    message.channel.send("There was an error communicating with the Tetrio API! (fix it <@501885489683628036>)");
-                    console.error("API Error:\n" + error);
-        
-                }
+          .setAuthor(message.author.username, message.author.avatarURL());
+          
+          
+          
 
-            } else {
-                message.channel.send('Invalid argument!, Please provide a user!');
-            }
-        }
-    },
+          if(country == "xm") var finalEmbed = finalEmbed.setTitle(username + " :new_moon: " + rankString);          //special line of code for user "osk" b/c the devs wanted to be special :/
+          
+          //after immense amounts of testing and working, this line of code worked for everyone except arcinferno, so i reworked it in a really dumb way, then it pings arc and says fuck you.
+          if(country == 'hidden') {
+            var finalEmbed = finalEmbed.setTitle(username + " " + "<:" + fuckArc + "_:"+ rankEmoji.id +">"); //sees if the country is hidden, displayes it propperly
+            message.channel.send("fuck you, <@247171493555994625>")
+          }
+          
+          
+          message.channel.send(finalEmbed);
+
+        })
+ 
+        .catch(error => {
+ 
+          console.log(message.channel.author + " tried a command with an invalid user!\nerr:\n" + error);
+ 
+          message.channel.send("Invalid User! (or API is having issues)"); 
+ 
+        }) 
+ 
+      } else {
+ 
+        message.channel.send('Invalid argument!, Please provide a user!');
+ 
+      }
+    }
+  },
 };
--- index.js
+++ index.js
@@ -2,55 +2,63 @@
 //spaghetti code belo
 
 const fs = require('fs');
-const { prefix, token } = require('./botconfig.json');
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const { prefix, token } = require('./botconfig.json');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 client.commands = new Discord.Collection();
+var killet;
+var defaultEmbed;
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
+
+
+
 client.on('ready', () => {
+
     console.log(`Logged in as ${client.user.tag}!\nKilletbot Development HQ :tm:`);
+    killet = client.users.cache.find(u => u.id === "501885489683628036");
+
+    
+    defaultEmbed = new Discord.MessageEmbed()
+    .setColor('#681482')
+    .setURL('https://tetr.io/')
+    .setTimestamp()
+    .setFooter( 'KilletBot™ HQ',  killet.avatarURL());
+
+
 });
 
 client.on("message", async message => {
-
-    //checks for killet and if starts with >, stops code execution if not.
-    if(!message.author.id == "501885489683628036") return;
+  
+    //checks for killet and if starts with >, stops code execution if not
+    if(message.author.id !== "501885489683628036" && message.author.id !== "263922052468178944") return;
     if (!message.content.startsWith(prefix)) return;
 
-    var cmdmsg = message.content;
-
-    //logs the command that was ran
-    console.log("Command entered by: " + message.author + " aka (" + message.author.username  + ") in #" + message.channel.name + " contents: " + message.content);
-
-    const args = message.content.split(/ +/);
-
-    console.log("args" + args);
-
+    var cmdmsg = message.content.toLowerCase();
+    const args = cmdmsg.split(/ +/); //sets up arguments for dynamic user-input
     cmdmsg = cmdmsg.replace(prefix, '');
     var command = cmdmsg.split(" ");
 
-    console.log("command" + command);
+    console.log("Command entered by: " + message.author + " aka (" + message.author.username  + ") in #" + message.channel.name + " contents: " + cmdmsg);    //logs the command that was ran
 
-
-    //IF THE COMMAND THAT A USER SENDS DOES NOT EXIST YET THEN IT WILL END THE CODE EARLY
-    if (!client.commands.has(command[0])) return;
+    if (!client.commands.has(command[0])) return; //checks if the command is valid, exits if not
 
     try {
-        client.commands.get(command[0]).execute(message, args);
-        console.log("thru2");
+
+        client.commands.get(command[0]).execute(message, args, defaultEmbed, client);
+
     } catch (error) {
+
         console.error(error);
+
         message.channel.send('There was an error trying to execute that command!');
+
     }
 
-    
 });
-
-
 
 client.login(token);
